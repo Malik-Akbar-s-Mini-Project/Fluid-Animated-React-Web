d752eedc9929cbd1fb9b63104a926ff8722bf4a2
Malik Akbar Hashemi Rafsanjani <pro.malikakbar2357@gmail.com>
2021-09-20 20:07:40 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./Navbar";
+import Title from "./Title";
+import Clearer from "./Clearer";
+import First from "./First";
+import Second from "./Second";
+import Third from "./Third";
+import Last from "./Last";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/">
+            <Navbar />
+            <Clearer />
+            <Title />
+            <First />
+            <Second />
+            <Third />
+            <Last />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
